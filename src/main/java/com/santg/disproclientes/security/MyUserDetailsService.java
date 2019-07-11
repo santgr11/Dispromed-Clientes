fa387ead57a99bd2631bcfb2ca2238f9baf19b25
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-11 20:51:56 -0300

    Update MyUserDetailsService.java

--- src/main/java/com/santg/disproclientes/security/MyUserDetailsService.java
+++ src/main/java/com/santg/disproclientes/security/MyUserDetailsService.java
@@ -1,15 +1,25 @@
 package com.santg.disproclientes.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.santg.disproclientes.entity.Vendedor;
+import com.santg.disproclientes.repository.VendedorRepository;
+
 public class MyUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private VendedorRepository vendedorRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Vendedor vendedor = vendedorRepository.findByNombre(username);
+		UserDetailsImpl userDetails = new UserDetailsImpl(vendedor);
+		
+		return userDetails;
 	}
 
 }
