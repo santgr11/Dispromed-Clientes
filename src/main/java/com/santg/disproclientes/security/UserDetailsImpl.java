0f9638b0add7a51300a6f5f0f8ed2db6edd2ee68
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-11 20:52:01 -0300

    Create UserDetailsImpl.java

---
+++ src/main/java/com/santg/disproclientes/security/UserDetailsImpl.java
@@ -0,0 +1,64 @@
+package com.santg.disproclientes.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.santg.disproclientes.entity.Vendedor;
+
+public class UserDetailsImpl implements UserDetails {
+
+	private Vendedor vendedor;
+
+	public UserDetailsImpl(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		// add the list of roles
+		this.vendedor.getRolesList().forEach(p -> {
+			GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + p);
+			authorities.add(authority);
+		});
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return vendedor.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return vendedor.getNombre();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
