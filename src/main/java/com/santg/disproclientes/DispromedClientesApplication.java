026c793f3e1d2ecc48818e490fc87f1b7d08056b
Santiago Grangetto <santiagograngetto@gmail.com>
2019-07-11 23:16:45 -0300

    set up postresql

--- src/main/java/com/santg/disproclientes/DispromedClientesApplication.java
+++ src/main/java/com/santg/disproclientes/DispromedClientesApplication.java
@@ -1,14 +1,29 @@
 package com.santg.disproclientes;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.santg.disproclientes.entity.Vendedor;
+import com.santg.disproclientes.repository.VendedorRepository;
 
 @SpringBootApplication
 public class DispromedClientesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DispromedClientesApplication.class, args);
+	}
+	
+	@Bean
+	ApplicationRunner applicationRunner(VendedorRepository vendedorRepository) {
+		
+		return args -> {
+			vendedorRepository.save(new Vendedor("jefe", "$2a$10$70cGClabHK6odYxKCoM1fe0M9GqLEVVyLUk1NwghUFowdLuOo.Uia", "JEFE,VENDEDOR"));
+			vendedorRepository.save(new Vendedor("vendedor", "$2a$10$70cGClabHK6odYxKCoM1fe0M9GqLEVVyLUk1NwghUFowdLuOo.Uia", "VENDEDOR"));
+
+		};
 	}
 
 }
